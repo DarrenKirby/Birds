31481414ae95b6065f1e9dcc7a577911bee120af
Darren Kirby <bulliver@gmail.com>
2025-04-13 08:05:51 -0700

    Create Shape.kt to style Card

---
+++ app/src/main/java/ca/dragonbyte/birds/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package ca.dragonbyte.birds.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(50.dp),
+    // for the card...
+    medium = RoundedCornerShape(bottomStart = 16.dp, topEnd = 16.dp)
+)
